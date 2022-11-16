c499168d866d2f95792b19301bbf89d276b111f2
Bryan Paronto <bryan@kindsys.us>
2022-11-15 21:22:55 -0600

    troubleshooting

--- cablecar/features/emacs.scm
+++ cablecar/features/emacs.scm
@@ -27,55 +27,6 @@
   #:use-module (cablecar packages emacs-xyz)
   #:export (%cablecar-base-emacs-packages))
 
-(define* (make-emacs-feature base-name
-                             #:key
-                             (home-services (const '()))
-                             (system-services (const '())))
-  "Creates a basic emacs feature configuration."
-  (let ((f-name (symbol-append 'emacs- base-name)))
-    (feature
-     (name f-name)
-     (values `((,f-name . #t)))
-     (home-services-getter home-services)
-     (system-services-getter system-services))))
-
-
-;; (define* (feature-emacs-exwm)
-;;   "Add and configure EXWM for Emacs."
-;;   (define emacs-f-name 'exwm)
-
-;;   (define (get-home-services config)
-;;     (list
-;;      (rde-elisp-configuration-service
-;;       emacs-f-name
-;;       config
-;;       `((require 'exwm)
-;;         ;; (defun bp/run-in-background (command)
-;;         ;;   (let ((command-parts (split-string command "[ ]+")))
-;;         ;;     (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
-
-;;         (defun exwm-async-run (name)
-;;           "Run a process asynchronously"
-;;           (interactive)
-;;           (start-process name nil name))
-
-;;         (defun bp/exwm-init-hook ()
-
-;;           (modify-all-frames-parameters
-;;            '((right-divider-width . 24)
-;;              (alpha . (85 . 85))
-;;              (mouse-color . "white")
-;;              (internal-border-width . 24)))
-;;           )
-;;         (exwm-enable)
-;;         )
-;;       #:elisp-packages (list
-;;                         cablecar-emacs-exwm emacs-desktop-environment))))
-
-;;   (make-emacs-feature emacs-f-name
-;;                       #:home-services get-home-services))
-
-
 (define* (rde-emacs-configuration-package
           name
           #:optional (elisp-expressions '())
@@ -98,15 +49,71 @@ dependency for other packages."
            #:authors (or authors '("Andrew Tropin <andrew@trop.in>")))))
       configure-package))
 
-(define emacs-configure-rde-keymaps
-  (rde-emacs-configuration-package
-   'rde-keymaps
-   `((defvar rde-app-map nil "Prefix keymap for applications.")
-     (define-prefix-command 'rde-app-map nil)
-     (defvar rde-toggle-map nil "\
-Prefix keymap for binding various minor modes for toggling functionalitty.")
-     (define-prefix-command 'rde-toggle-map nil))
-   #:summary "Keymaps intended for reuse among configure-* packages"))
+(define* (rde-elisp-configuration-service
+          name config
+          #:optional (elisp-expressions '())
+          #:key
+          summary authors url keywords commentary
+          (early-init '())
+          (elisp-packages '()))
+  "Adds a configure-NAME package to the profile and emacs load path and if
+emacs-portable? rde value is present adds autoloads cookies to each expression
+of it, otherwise adds a require to @file{init.el}."
+  (let* ((pkg-name (symbol-append 'configure- name))
+         (autoloads? (get-value 'emacs-portable? config))
+         (require-in-init? (not autoloads?))
+         (configure-package
+          (rde-emacs-configuration-package
+           name elisp-expressions
+           #:summary summary
+           #:commentary commentary
+           #:keywords keywords
+           #:url url
+           #:authors authors
+           #:elisp-packages elisp-packages
+           #:autoloads? autoloads?)))
+    (simple-service
+     (symbol-append 'emacs- name '-configurations)
+     home-emacs-service-type
+     (home-emacs-extension
+      (init-el (if require-in-init? `((require ',pkg-name)) '()))
+      (early-init-el early-init)
+      ;; It's necessary to explicitly add elisp-packages here, because
+      ;; we want to overwrite builtin emacs packages.  Propagated
+      ;; inputs have lowest priority on collisions, that's why we have
+      ;; to list those package here in addition to propagated-inputs.
+      (elisp-packages (append elisp-packages (list configure-package)))))))
+
+;; MAYBE: make handler to be actions instead of desktop entries?
+(define* (emacs-xdg-service
+          name xdg-name gexp
+          #:key
+          (default-for '())
+          (exec-argument "%u"))
+  (define file-name (string-append "emacs-" (symbol->string name)))
+  (define file-file (file-append (program-file file-name gexp)
+                                 (string-append " " exec-argument)))
+  (define desktop-file (symbol-append 'emacs- name '.desktop))
+  (simple-service
+   (symbol-append 'emacs-xdg- name)
+   home-xdg-mime-applications-service-type
+   (home-xdg-mime-applications-configuration
+    (default (map (lambda (m) (cons m desktop-file)) default-for))
+    (desktop-entries
+     (list
+      (xdg-desktop-entry
+       (file file-name)
+       (name xdg-name)
+       (config `((exec . ,file-file)
+                 (icon . "emacs")))
+       (type 'application)))))))
+
+(define (expand-extra-elisp elisp config)
+  "If ELISP is a list just return it, if it's a function call it with CONFIG
+argument, throw an exception otherwise."
+  (let ((res (if (procedure? elisp) (elisp config) elisp)))
+    (ensure-pred list? res)
+    res))
 
 (define* (feature-emacs-cablecar
           #:key

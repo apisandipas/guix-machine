0c68a87e54e2f4eb0a0afc8b095a12a5dce105b3
Bryan Paronto <bryan@kindsys.us>
2022-11-18 07:46:55 -0600

    troubleshooting

--- cablecar/features/emacs.scm
+++ cablecar/features/emacs.scm
@@ -457,9 +457,9 @@ It can contain settings not yet moved to separate features."
   (list
    ;; (feature-emacs-exwm)
    ;; (feature-emacs-evil)
-   ;; (feature-emacs-appearance
-   ;;  #:dark? #t)
-   ;; (feature-emacs-faces)
+   (feature-emacs-appearance
+    #:dark? #t)
+   (feature-emacs-faces)
    ;; (feature-emacs-completion)
    ;; (feature-vterm)
    ;; (feature-emacs-vertico)

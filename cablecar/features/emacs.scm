3723d7c610b64ae00ebbf987edd0d32f0df70f5f
Bryan Paronto <bryan@kindsys.us>
2022-11-19 13:26:40 -0600

    troubleshooting

--- cablecar/features/emacs.scm
+++ cablecar/features/emacs.scm
@@ -651,39 +651,39 @@ It can contain settings not yet moved to separate features."
                 ([?\s-9] . (lambda () (interactive) (exwm-workspace-switch-create 8)))
                 ([?\s-0] . (lambda () (interactive) (exwm-workspace-switch-create 9)))))
 
-            ;; (require 'desktop-environment)
-            ;; (desktop-environment-mode)
-            ;; (desktop-environment-brightness-small-increment "2%+")
-            ;; (desktop-environment-brightness-small-decrement "2%-")
-            ;; (desktop-environment-brightness-normal-increment "5%+")
-            ;; (desktop-environment-brightness-normal-decrement "5%-")
+            (require 'desktop-environment)
+            (desktop-environment-mode)
+            (desktop-environment-brightness-small-increment "2%+")
+            (desktop-environment-brightness-small-decrement "2%-")
+            (desktop-environment-brightness-normal-increment "5%+")
+            (desktop-environment-brightness-normal-decrement "5%-")
 
 
-            ;; (defun exwm-input-line-mode ()
-            ;;   "Set exwm window to line-mode and show mode line"
-            ;;   (call-interactively #'exwm-input-grab-keyboard))
+            (defun exwm-input-line-mode ()
+              "Set exwm window to line-mode and show mode line"
+              (call-interactively #'exwm-input-grab-keyboard))
 
-            ;; (defun exwm-input-char-mode ()
-            ;;   "Set Exwm window to char-mode and hide mode line"
-            ;;   (call-interactively #'exwm-input-release-keyboard))
+            (defun exwm-input-char-mode ()
+              "Set Exwm window to char-mode and hide mode line"
+              (call-interactively #'exwm-input-release-keyboard))
 
-            ;; (defun exwm-input-toggle-mode ()
-            ;;   "Toggle between line- and char-mode"
-            ;;   (with-current-buffer (window-buffer)
-            ;;     (when (eq major-mode 'exwm-mode)
-            ;;       (if (equal (nth 1 (nth 1 mode-line-process)) "line")
-            ;;           (exwm-input-char-mode)
-            ;;         (exwm-input-line-mode)))))
+            (defun exwm-input-toggle-mode ()
+              "Toggle between line- and char-mode"
+              (with-current-buffer (window-buffer)
+                (when (eq major-mode 'exwm-mode)
+                  (if (equal (nth 1 (nth 1 mode-line-process)) "line")
+                      (exwm-input-char-mode)
+                    (exwm-input-line-mode)))))
 
-            ;; (exwm-input-set-key (kbd "s-i")
-            ;;                     (lambda () (interactive)
-            ;;                       (exwm-input-toggle-mode)))
+            (exwm-input-set-key (kbd "s-i")
+                                (lambda () (interactive)
+                                  (exwm-input-toggle-mode)))
 
-            ;; (exwm-input-set-key (kbd "s-o")
-            ;;                     (lambda ()
-            ;;                       (interactive)
-            ;;                       (exwm-input-toggle-mode)
-            ;;                       (org-capture)))
+            (exwm-input-set-key (kbd "s-o")
+                                (lambda ()
+                                  (interactive)
+                                  (exwm-input-toggle-mode)
+                                  (org-capture)))
 
         (exwm-enable)
         )

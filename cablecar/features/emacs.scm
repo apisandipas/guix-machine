36d624ab4293b484b68daf0f125a9a17c48c227f
Bryan Paronto <bryan@kindsys.us>
2022-11-19 13:06:09 -0600

    troubleshooting

--- cablecar/features/emacs.scm
+++ cablecar/features/emacs.scm
@@ -555,29 +555,14 @@ It can contain settings not yet moved to separate features."
       config
       '(
         (require 'exwm)
-        (defun bp/run-in-background (command)
-          (let ((command-parts (split-string command "[ ]+")))
-            (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+        ;; (defun bp/run-in-background (command)
+        ;;   (let ((command-parts (split-string command "[ ]+")))
+        ;;     (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
 
         (defun exwm-async-run (name)
           "Run a process asynchronously"
           (interactive)
           (start-process name nil name))
-
-        (defun bp/polybar-exwm-workspace ()
-          (interactive)
-          (pcase exwm-workspace-current-index
-                 (0 "  Video")
-                 (1 "  Term")
-                 (2 "  Chat")
-                 (3 "  Dev")
-                 (4 "  Mail")
-                 (5 "爵  Web")
-                 (6 "  VCS")
-                 (7 "  Music")
-                 (8 "  Files")
-                 (9 "  Streaming")))
-
 
         (defun bp/exwm-init-hook ()
 
@@ -592,14 +577,6 @@ It can contain settings not yet moved to separate features."
 
           ;; Useless gaps
           (exwm-outer-gaps-mode -1)
-
-          ;; Start the Polybar panel
-          ;; (bp/start-panel)
-
-          ;; Launch apps that will run in the background
-          ;; (bp/run-in-background "dropbox")
-          ;; (bp/run-in-background "nm-applet")
-          ;; (bp/run-in-background "pasystray")
           )
 
         (add-hook 'exwm-init-hook #'bp/exwm-init-hook)
@@ -618,46 +595,46 @@ It can contain settings not yet moved to separate features."
                 ?\C-\M-j ;; Buffer list
                 ?\C-\ )) ;; Ctrl+Space
 
-        (setq exwm-input-global-keys
-              `(
-                ;; Move between windows
-                ([s-left] . windmove-left)
-                ([s-right] . windmove-right)
-                ([s-up] . windmove-up)
-                ([s-down] . windmove-down)
+        ;; (setq exwm-input-global-keys
+        ;;       `(
+        ;;         ;; Move between windows
+        ;;         ([s-left] . windmove-left)
+        ;;         ([s-right] . windmove-right)
+        ;;         ([s-up] . windmove-up)
+        ;;         ([s-down] . windmove-down)
 
-                ;; ;; Launch applications via shell command
-                ([?\s-\\] . (lambda (command)
-                              (interactive (list (read-shell-command " ")))
-                              (start-process-shell-command command nil command)))
+        ;;         ;; ;; Launch applications via shell command
+        ;;         ([?\s-\\] . (lambda (command)
+        ;;                       (interactive (list (read-shell-command " ")))
+        ;;                       (start-process-shell-command command nil command)))
 
-                ;; Switch workspace
-                ;; ([?\s-w] . exwm-workspace-switch)
-                ;; ([?\s-w] . counsel-switch-buffer)
-                ;;
-                ;; move window workspace with SUPER+SHIFT+{0-9}
-                ,@(cl-mapcar (lambda (c n)
-                               `(,(kbd (format "s-%c" c)) .
-                                 (lambda ()
-                                   (interactive)
-                                   (exwm-workspace-move-window ,n)
-                                   ;; (exwm-workspace-switch ,n)
-                                   )))
-                             '(?! ?@ ?# ?$ ?% ?^ ?& ?* ?\( ?\))
-                             ;; '(?\) ?! ?@ ?# ?$ ?% ?^ ?& ?* ?\()
-                             (number-sequence 0 9))
+        ;;         ;; Switch workspace
+        ;;         ;; ([?\s-w] . exwm-workspace-switch)
+        ;;         ;; ([?\s-w] . counsel-switch-buffer)
+        ;;         ;;
+        ;;         ;; move window workspace with SUPER+SHIFT+{0-9}
+        ;;         ,@(cl-mapcar (lambda (c n)
+        ;;                        `(,(kbd (format "s-%c" c)) .
+        ;;                          (lambda ()
+        ;;                            (interactive)
+        ;;                            (exwm-workspace-move-window ,n)
+        ;;                            ;; (exwm-workspace-switch ,n)
+        ;;                            )))
+        ;;                      '(?! ?@ ?# ?$ ?% ?^ ?& ?* ?\( ?\))
+        ;;                      ;; '(?\) ?! ?@ ?# ?$ ?% ?^ ?& ?* ?\()
+        ;;                      (number-sequence 0 9))
 
-                ;; Switch to window workspace with SUPER+{0-9}
-                ([?\s-1] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
-                ([?\s-2] . (lambda () (interactive) (exwm-workspace-switch-create 1)))
-                ([?\s-3] . (lambda () (interactive) (exwm-workspace-switch-create 2)))
-                ([?\s-4] . (lambda () (interactive) (exwm-workspace-switch-create 3)))
-                ([?\s-5] . (lambda () (interactive) (exwm-workspace-switch-create 4)))
-                ([?\s-6] . (lambda () (interactive) (exwm-workspace-switch-create 5)))
-                ([?\s-7] . (lambda () (interactive) (exwm-workspace-switch-create 6)))
-                ([?\s-8] . (lambda () (interactive) (exwm-workspace-switch-create 7)))
-                ([?\s-9] . (lambda () (interactive) (exwm-workspace-switch-create 8)))
-                ([?\s-0] . (lambda () (interactive) (exwm-workspace-switch-create 9)))))
+        ;;         ;; Switch to window workspace with SUPER+{0-9}
+        ;;         ([?\s-1] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
+        ;;         ([?\s-2] . (lambda () (interactive) (exwm-workspace-switch-create 1)))
+        ;;         ([?\s-3] . (lambda () (interactive) (exwm-workspace-switch-create 2)))
+        ;;         ([?\s-4] . (lambda () (interactive) (exwm-workspace-switch-create 3)))
+        ;;         ([?\s-5] . (lambda () (interactive) (exwm-workspace-switch-create 4)))
+        ;;         ([?\s-6] . (lambda () (interactive) (exwm-workspace-switch-create 5)))
+        ;;         ([?\s-7] . (lambda () (interactive) (exwm-workspace-switch-create 6)))
+        ;;         ([?\s-8] . (lambda () (interactive) (exwm-workspace-switch-create 7)))
+        ;;         ([?\s-9] . (lambda () (interactive) (exwm-workspace-switch-create 8)))
+        ;;         ([?\s-0] . (lambda () (interactive) (exwm-workspace-switch-create 9)))))
 
             (require 'desktop-environment)
             (desktop-environment-mode)
